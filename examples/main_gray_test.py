0ed94d24144eee8c6385a839a9874b83d2e8dfc0
Rinat Mukhometzianov <25082858+rmukh@users.noreply.github.com>
2022-06-02 21:07:26 -0400

    example added

---
+++ examples/main_gray_test.py
@@ -0,0 +1,46 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import tv_flow_python
+
+# prepare variables for TV flow
+T = 10 # diffusion time
+dt = 1 / 50 # diffusion step
+
+NOB = int(np.ceil(T / dt))
+
+NIT = 100 # number of iteration for TV denoising convergence
+tol = 1e-5 # convergence tolerance
+
+lami = 2 * dt
+
+im_raw = Image.open('fruits.bmp')
+f = im_raw.convert('L') # to gray-scale
+f = np.array(f)
+
+n, m = f.shape
+
+# Normalize image
+f = f / 255.0
+f = 5.0 * f
+
+f_F = np.asfortranarray(f) # to Eigen3 format 
+
+# Run...
+# Measure an execution time of the code below
+start = time.time()
+S = tv_flow_python.run_TV_flow(f_F, n, m, NOB, lami, dt, tol, NIT)
+end = time.time()
+
+print(S)
+print('Time: ', end - start)
+
+t = 2 * dt + np.arange(NOB) * dt
+
+# Plot spectral response
+plt.plot(t, S)
+plt.ylabel('$\sum|\phi(n)|$')
+plt.xlabel('$t$')
+
+plt.show()
